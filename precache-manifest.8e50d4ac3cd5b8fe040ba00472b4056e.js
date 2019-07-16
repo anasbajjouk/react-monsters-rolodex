69880b6f7a34f2a02b963339183024efdc52b542
Anas Bajjouk <bajjouk1@gmail.com>
2019-07-16 15:07:06 +0200

    Updates

---
+++ precache-manifest.8e50d4ac3cd5b8fe040ba00472b4056e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2ab7a10620d7e8a4a3da2907ffb836f",
+    "url": "/react-monsters-rolodex/index.html"
+  },
+  {
+    "revision": "4e974bfcc6bea4b0313d",
+    "url": "/react-monsters-rolodex/static/css/main.23634b31.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/react-monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "4e974bfcc6bea4b0313d",
+    "url": "/react-monsters-rolodex/static/js/main.3acd7c19.chunk.js"
+  },
+  {
+    "revision": "09fc101723e82bb4a8f4",
+    "url": "/react-monsters-rolodex/static/js/runtime~main.2a38a34c.js"
+  }
+]);
